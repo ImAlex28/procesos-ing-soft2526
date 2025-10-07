f0476d7939b293f0ee36b6e57da898672820c3b1
Alejandro G. <115694318+ImAlex28@users.noreply.github.com>
2025-10-07 17:30:51 +0200

    PROC-15 Implementar getUsers

--- client/model.js
+++ client/model.js
@@ -3,6 +3,9 @@ function System() {
   this.addUser = function (nick) {
     this.users[nick] = new User(nick);
   };
+  this.getUsers = function () {
+    return this.users;
+  };
 }
 function User(nick) {
   this.nick = nick;
