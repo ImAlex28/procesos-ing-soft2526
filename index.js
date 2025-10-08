ba5478c168a00eb86e95b61e5c2f8c8a5b42245c
ImAlex28 <115694318+ImAlex28@users.noreply.github.com>
2025-10-08 23:34:58 +0200

    PROC-3  Integrar el modelo en nuestro servidor, conectar cliente y el servidor

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 const express = require('express');
 const app = express();
-//const modelo = require("./servidor/modelo.js"); 
+const model = require("./server/model.js"); 
 const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/"));
 app.get("/", function (request, response) {
     response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hola Mundo!');
+    let system = new model.System();
+    var contenido=fs.readFileSync(__dirname+"/client/index.html"); 
+    response.setHeader("Content-type","text/html"); 
+    response.send(contenido); 
 });
 app.listen(PORT, () => {
     console.log(`App está escuchando en el puerto ${PORT}`);
---
+++ server/model.js
@@ -0,0 +1,32 @@
+function System() {
+  this.users = {};
+  this.addUser = function (nick) {
+    this.users[nick] = new User(nick);
+  };
+  this.getUsers = function () {
+    return this.users;
+  };
+  this.activeUser = function (nick) {
+    return this.users.hasOwnProperty(nick);
+    //hasOwnProperty(nick) comprueba si el objeto users tiene una propiedad con ese nombre.
+  };
+  this.deleteUser = function (nick) {
+    if (this.activeUser(nick)) {
+      delete this.users[nick];
+      return true; // Eliminación exitosa
+    }
+    return false; // Usuario no encontrado
+  };
+  this.usersNumber = function () {
+    return Object.keys(this.users).length 
+  }
+}
+
+module.exports.System=System;
+
+function User(nick) {
+  this.nick = nick;
+}
+
+
+
