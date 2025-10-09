b1695f241ad801e04b47b14113c76105b9996a58
ImAlex28 <115694318+ImAlex28@users.noreply.github.com>
2025-10-09 18:37:20 +0200

    PROC-3 Implementar la API REST en index.js

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const fs = require("fs");
 const express = require('express');
 const app = express();
-const model = require("./server/model.js"); 
+const model = require("./server/model.js");
 const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/"));
+let system = new model.System();
+
 app.get("/", function (request, response) {
     response.statusCode = 200;
-    let system = new model.System();
-    var contenido=fs.readFileSync(__dirname+"/client/index.html"); 
-    response.setHeader("Content-type","text/html"); 
-    response.send(contenido); 
+    var contenido = fs.readFileSync(__dirname + "/client/index.html");
+    response.setHeader("Content-type", "text/html");
+    response.send(contenido);
 });
+
+app.get("/addUser/:nick", function (request, response) {
+    let nick = request.params.nick;
+    let res = system.addUser(nick);
+    response.send(res);
+});
+
+app.get("/getUsers/", function (request,response) {
+    let res = system.getUsers();
+    response.send(res);
+});
+
+app.get("/activeUser/:nick", function(request, response){
+    let nick = request.params.nick;
+    let res = system.activeUser(nick);
+    response.send(res);
+})
+
+app.get("/usersNumber/", function(request, response){
+    let res = system.usersNumber();
+    response.send(res);
+})
+
+app.get("/deleteUser/:nick", function(request, response) {
+    let nick = request.params.nick;
+    let res = system.deleteUser(nick);
+    response.send(res);
+})
+
 app.listen(PORT, () => {
     console.log(`App está escuchando en el puerto ${PORT}`);
     console.log('Ctrl+C para salir');
--- server/model.js
+++ server/model.js
@@ -1,15 +1,26 @@
 function System() {
   this.users = {};
   this.addUser = function (nick) {
-    this.users[nick] = new User(nick);
+      let res = {"nick":- 1};
+      if (!this.users[nick]) {
+        this.users[nick] = new User(nick);
+        res.nick = nick;
+      }
+      else {
+        console.log("el nick " + nick + " está en uso");
+      }
+      return res;
   };
+
   this.getUsers = function () {
     return this.users;
   };
+
   this.activeUser = function (nick) {
     return this.users.hasOwnProperty(nick);
     //hasOwnProperty(nick) comprueba si el objeto users tiene una propiedad con ese nombre.
   };
+
   this.deleteUser = function (nick) {
     if (this.activeUser(nick)) {
       delete this.users[nick];
@@ -17,12 +28,16 @@ function System() {
     }
     return false; // Usuario no encontrado
   };
+
   this.usersNumber = function () {
-    return Object.keys(this.users).length 
+    let res = {"num":- 1};
+    number = Object.keys(this.users).length
+    res.num = number
+    return res
   }
 }
 
-module.exports.System=System;
+module.exports.System = System;
 
 function User(nick) {
   this.nick = nick;
