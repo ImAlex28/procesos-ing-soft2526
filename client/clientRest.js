632893487e173eec1d9bede1f8ccd4700c90e6de
ImAlex28 <115694318+ImAlex28@users.noreply.github.com>
2025-10-10 00:58:06 +0200

    PROC-5  Implementar el cliente de comunicación Rest

---
+++ client/clientRest.js
@@ -0,0 +1,102 @@
+function ClientRest() {
+    this.addUser = function (nick) {
+        var cli = this;
+        $.getJSON("/addUser/" + nick, function (data) {
+            if (data.nick != -1) {
+                console.log("Usuario " + nick + " ha sido registrado")
+            }
+            else {
+                console.log("El nick ya está ocupado");
+            }
+        })
+    }
+
+    this.addUserAlternative = function (nick) {
+        $.ajax({
+            type: 'GET',
+            url: '/addUser/' + nick,
+            success: function (data) {
+                if (data.nick != -1) {
+                    console.log("Usuario " + nick + " ha sido registrado")
+                }
+                else {
+                    console.log("El nick ya está ocupado");
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+                console.error("Error: " + errorThrown);
+            },
+            contentType: 'application/json'
+        });
+    }
+
+    this.getUsers = function () {
+        $.ajax({
+            type: 'GET',
+            url: '/getUsers',
+            success: function (data) {
+                console.log(JSON.stringify(data, null, 2));
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.error('❌ Error:', errorThrown);
+            },
+            contentType: 'application/json'
+        });
+    };
+
+    this.usersNumber = function () {
+        $.ajax({
+            type: 'GET',
+            url: '/usersNumber',
+            success: function (data) {
+                console.log(JSON.stringify(data, null, 2));
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.error('❌ Error:', errorThrown);
+            },
+            contentType: 'application/json'
+        });
+    };
+
+    this.activeUser = function (nick) {
+        $.ajax({
+            type: 'GET',
+            url: '/activeUser/' + nick,
+            success: function (data) {
+                if (data) {
+                    console.log("Usuario " + nick + " activo.")
+                }
+                else {
+                    console.log("El usuario no existe.");
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+                console.error("Error: " + errorThrown);
+            },
+            contentType: 'application/json'
+        });
+    }
+
+    this.deleteUser = function (nick) {
+        $.ajax({
+            type: 'GET',
+            url: '/deleteUser/' + nick,
+            success: function (data) {
+                if (data) {
+                    console.log("Usuario " + nick + " eliminado.")
+                }
+                else {
+                    console.log("El usuario no existe.");
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+                console.error("Error: " + errorThrown);
+            },
+            contentType: 'application/json'
+        });
+    }
+
+} 
